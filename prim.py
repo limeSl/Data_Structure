304c4214d95f640d401d03542767c2147e8077d9
limeSl <wcdustn@naver.com>
2022-05-13 20:52:38 +0900

    프림 알고리즘 구현

--- prim.py
+++ prim.py
@@ -7,22 +7,22 @@ prim의 방식으로 최소 비용 신장 트리 찾기
 - 신장 트리에 간선이 추가되는 순서대로 출력
 
 '''
-class Node:
-    def __init__(self, data = None):
-        self.data = data
-        self.edge_list=[]
 
 class Graph:
     def __init__(self):
         self.graph = {}
         self.v_list = []
+        self.e_list = []
         self.edge = []
         self.total = 0
 
     def create(self, v1, v2, weight):
         if v1 not in self.graph:
             self.graph[v1] = []
-        self.graph[v1].append((v2, weight))
+        if v2 not in self.graph:
+            self.graph[v2] = []
+        self.graph[v1].append((v1, v2, weight))
+        self.graph[v2].append((v2, v1, weight))
 
     def sort_edge(self, network):
         temp = []
@@ -30,14 +30,6 @@ class Graph:
             temp.append((cost, v1 ,v2))
         temp.sort()
         return temp
-    
-    '''
-    def find(self, v2):
-        for v1, lst in self.v_list.items():
-            if v2 == v1: return v1
-            if v2 in self.v_list[v1]: return v1
-        return -1
-    '''
 
     def union(self, s1, s2):
         if s1 < s2:
@@ -51,17 +43,35 @@ class Graph:
 
     def prim(self, start):
         self.v_list.append(start)
-        print('set list=', self.v_list)
-        sort_network = self.sort_edge(network)
-        for cost, v1, v2 in sort_network:
-            s1 = self.find(v1)
-            s2 = self.find(v2)
-            if s1 == s2:
+        print('Spanning Tree =', self.v_list)
+        self.e_list.extend(self.graph[start])
+        self.e_list = self.sort_edge(self.e_list)
+
+        while self.e_list:
+            cost = self.e_list[0][0]
+            v1 = self.e_list[0][1]
+            v2 = self.e_list[0][2]
+            print('\n(',v1,',',v2,')','cost = ',cost)
+            del self.e_list[0]
+
+            if v1 in self.v_list and v2 in self.v_list:
+                print("rejected for cycle")
                 continue
+            
             self.edge.append((v1, v2, cost))
             self.total += cost
-            self.union(s1, s2)
-        
+
+            extend_list = []
+            for s1, s2, cost in self.graph[v2]:
+                if s2 not in self.v_list:
+                    extend_list.append((s1, s2, cost))
+            extend_list = self.sort_edge(extend_list)
+
+            self.e_list.extend(extend_list)
+            self.e_list.sort()
+            self.v_list.append(v2)
+            
+            print('Spanning Tree =',self.v_list)
 
 g = Graph()
 # (v1, v2, weight)
@@ -70,15 +80,13 @@ node_list = []
 
 for v1, v2, weight in network:
     g.create(v1, v2, weight)
-    if node_list[0]//2 == 0:
-        pass
-
 
 print('network=', network)
-
-start = input("start node : ")
-g.prim(start)
 print(g.graph)
-print(g.v_list)
-print(g.edge)
-print(g.total)
+
+start = int(input("start node : "))
+g.prim(start)
+print()
+print('Spanning tree vertices = ', g.v_list)
+print('spanning tree edges = ', g.edge)
+print('cost total = ', g.total)
